472c3d82b5ef416641641ee3cd3a956f5c63251f
Jacob Lam <122247726+jacoblam121@users.noreply.github.com>
2024-10-01 18:45:35 +0000

    edited hello.c again

--- hello.c
+++ hello.c
@@ -3,11 +3,11 @@
 int main() {
   char hello[] = "Hello!"; 
   char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+  char edit[] = "edit";
 
   puts(hello);
   puts(hello2);
+  puts(edit);
 
   return 0;
 }
-
-//making an edit
